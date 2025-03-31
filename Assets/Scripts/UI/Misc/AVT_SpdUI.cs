fc72685550c63916a9d57b8af8825b704c4f518c
nam tran trong <nam.tran-trong@ubisoft.com>
2025-03-31 22:17:40 +0700

    update

--- Assets/Scripts/UI/Misc/AVT_SpdUI.cs
+++ Assets/Scripts/UI/Misc/AVT_SpdUI.cs
@@ -4,43 +4,102 @@ using UnityEngine.UI;
 
 public class AVT_SpdUI : MonoBehaviour
 {
-    [Title("Image")]
-    public Image background;
-    public Image icon;
+    #region Serialized Fields
+    [Title("UI Components")]
+    [SerializeField] private Image background;
+    [SerializeField] private Image icon;
+    [SerializeField] private GameObject startObject;
+    [SerializeField] private GameObject focusObject;
 
-    [Title("Sprite")] 
-    public Sprite main;
-    public Sprite enemy;
-    public Sprite team;
-    public GameObject startObject;
-    public GameObject focusObject;
+    [Title("Background Sprites")] 
+    [SerializeField] private Sprite mainCharacterSprite;
+    [SerializeField] private Sprite enemySprite;
+    [SerializeField] private Sprite teamMemberSprite;
+    #endregion
+
+    #region Private Fields
     private Character _character;
     private TopBar_UI _topBar;
-    
-    public void SetupUI(Character character, TopBar_UI topBar, bool isStartObject)
+    #endregion
+
+    #region Properties
+    public Character Character => _character;
+    #endregion
+
+    #region Public Methods
+    public void SetupUI(Character character, TopBar_UI topBar, bool isStartCharacter)
     {
-        _topBar = topBar;
-        startObject.SetActiveIfNeeded(isStartObject);
+        if (character == null)
+        {
+            Debug.LogError("Attempted to setup AVT_SpdUI with null character");
+            return;
+        }
+
+        UnsubscribeFromCurrentCharacter();
+        InitializeNewCharacter(character, topBar);
+        UpdateVisuals(character, isStartCharacter);
+    }
+    #endregion
+
+    #region Private Methods
+    private void UnsubscribeFromCurrentCharacter()
+    {
         if (_character != null)
         {
-            _character.OnDeath -= OnDeath;
+            _character.OnDeath -= HandleCharacterDeath;
         }
+    }
+
+    private void InitializeNewCharacter(Character character, TopBar_UI topBar)
+    {
         _character = character;
-        _character.OnDeath += OnDeath;
-        icon.sprite = _character.characterConfig.slideBarIcon;
-        background.sprite = character.IsMainCharacter ? main : (character.Type == Type.AI ? enemy : team);
+        _topBar = topBar;
+        _character.OnDeath += HandleCharacterDeath;
+    }
+
+    private void UpdateVisuals(Character character, bool isStartCharacter)
+    {
+        startObject.SetActiveIfNeeded(isStartCharacter);
+        UpdateCharacterIcon(character);
+        UpdateBackgroundSprite(character);
+        UpdateFocusIndicator(character);
+    }
+
+    private void UpdateCharacterIcon(Character character)
+    {
+        icon.sprite = character.characterConfig.slideBarIcon;
+    }
+
+    private void UpdateBackgroundSprite(Character character)
+    {
+        background.sprite = GetBackgroundSpriteForCharacter(character);
+    }
+
+    private Sprite GetBackgroundSpriteForCharacter(Character character)
+    {
+        if (character.IsMainCharacter) return mainCharacterSprite;
+        return character.Type == Type.AI ? enemySprite : teamMemberSprite;
+    }
+
+    private void UpdateFocusIndicator(Character character)
+    {
         focusObject.SetActive(character.IsMainCharacter);
     }
 
-    private void OnDestroy()
+    private void HandleCharacterDeath(object sender, Character character)
     {
-        if (_character != null)
-            _character.OnDeath -= OnDeath;
-    }
-
-    private void OnDeath(object sender, Character character)
-    {
-        _topBar.DestroyAvt(this);
+        if (_topBar != null)
+        {
+            _topBar.DestroyAvt(this);
+        }
         Destroy(gameObject);
     }
+    #endregion
+
+    #region Unity Lifecycle
+    private void OnDestroy()
+    {
+        UnsubscribeFromCurrentCharacter();
+    }
+    #endregion
 }
--- Assets/Scripts/UI/Misc/TopBar_UI.cs
+++ Assets/Scripts/UI/Misc/TopBar_UI.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class TopBar_UI : MonoBehaviour
 {
@@ -10,21 +11,32 @@ public class TopBar_UI : MonoBehaviour
     [SerializeField] private float unfocusedScale = 0.7f;
 
     private readonly List<AVT_SpdUI> _avtSpdUI = new();
-
     private GameplayManager Gameplay => GameplayManager.Instance;
 
-    public void Awake()
+    #region Unity Lifecycle
+    private void Awake()
     {
         ClearAllChildren();
         GameManager.Instance.OnMainCharacterChanged += UpdateTopBar;
     }
 
-    public void OnDestroy()
+    private void OnDestroy()
     {
         if (GameManager.HasInstance)
             GameManager.Instance.OnMainCharacterChanged -= UpdateTopBar;
     }
+    #endregion
 
+    #region Public Methods
+    public void DestroyAvt(AVT_SpdUI avt)
+    {
+        if (avt == null || !_avtSpdUI.Remove(avt)) return;
+        Destroy(avt.gameObject);
+        SetUI();
+    }
+    #endregion
+
+    #region Private Methods
     private void UpdateTopBar()
     {
         TryInitUI();
@@ -33,57 +45,191 @@ public class TopBar_UI : MonoBehaviour
 
     private void TryInitUI()
     {
-        if (_avtSpdUI.Count == Gameplay.Characters.Count) return;
+        try 
+        {
+            ValidateRequiredComponents();
+            if (_avtSpdUI.Count == Gameplay.Characters.Count) return;
+            
+            ReinitializeAvatars();
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+    private void ValidateRequiredComponents()
+    {
+        if (Gameplay == null)
+            throw new System.NullReferenceException("GameplayManager.Instance is null");
+
+        if (Gameplay.Characters == null)
+            throw new System.NullReferenceException("GameplayManager.Characters is null");
+
+        if (characterPool == null)
+            throw new System.NullReferenceException("characterPool Transform is null");
+
+        if (avatarPrefab == null)
+            throw new System.NullReferenceException("avatarPrefab is null");
+
+        if (_avtSpdUI == null)
+            throw new System.NullReferenceException("_avtSpdUI List is null");
+    }
+
+    private void ReinitializeAvatars()
+    {
         _avtSpdUI.Clear();
         ClearAllChildren();
+        
         foreach (var character in Gameplay.Characters)
         {
-            if (character == null) continue;
-            var go = Instantiate(avatarPrefab, characterPool);
-            if (go == null) continue;
-            var rt = go.GetComponent<RectTransform>();
-            if (rt != null)
+            if (character == null)
             {
-                rt.anchorMin = new Vector2(0, 0.5f);
-                rt.anchorMax = new Vector2(0, 0.5f);
-                rt.pivot = new Vector2(0.5f, 0.5f);
+                Debug.LogException(new System.NullReferenceException("Character in Characters list is null"));
+                continue;
             }
-            var avtSpd = go.GetComponent<AVT_SpdUI>();
-            if (avtSpd != null)
-            {
-                _avtSpdUI.Add(avtSpd);
-            }
-            else
-            {
-                Destroy(go);
-            }
+            
+            CreateAvatarForCharacter(character);
         }
+    }
+
+    private void CreateAvatarForCharacter(Character character)
+    {
+        var go = Instantiate(avatarPrefab, characterPool);
+        if (!ValidateAvatarGameObject(go)) return;
+
+        var rt = SetupRectTransform(go);
+        if (rt == null) return;
+
+        var avtSpd = go.GetComponent<AVT_SpdUI>();
+        if (avtSpd == null)
+        {
+            Debug.LogException(new System.Exception($"AVT_SpdUI component missing on {go.name}"));
+            Destroy(go);
+            return;
+        }
+
+        _avtSpdUI.Add(avtSpd);
+    }
+
+    private bool ValidateAvatarGameObject(GameObject go)
+    {
+        if (go != null) return true;
+        
+        Debug.LogException(new System.Exception("Failed to instantiate avatarPrefab"));
+        return false;
+    }
+
+    private RectTransform SetupRectTransform(GameObject go)
+    {
+        var rt = go.GetComponent<RectTransform>();
+        if (rt == null)
+        {
+            Debug.LogException(new System.Exception($"RectTransform component missing on {go.name}"));
+            Destroy(go);
+            return null;
+        }
+
+        rt.anchorMin = new Vector2(0, 0.5f);
+        rt.anchorMax = new Vector2(0, 0.5f);
+        rt.pivot = new Vector2(0.5f, 0.5f);
+        return rt;
     }
 
     private void SetUI()
     {
-        var count = _avtSpdUI.Count;
-        if (count == 0) return;
+        try
+        {
+            if (!ValidateUISetup(out var count)) return;
 
-        var startIndex = Gameplay.CurrentPlayerIndex;
-        var fixedY = _avtSpdUI.Count > 0 ? _avtSpdUI[0].GetComponent<RectTransform>().anchoredPosition.y : 0f;
+            ReorderAvatars();
+
+            var currentIndex = Gameplay.CurrentPlayerIndex;
+            var startIndex = currentIndex;
+            
+            if (currentIndex >= count)
+            {
+                startIndex = currentIndex % count;
+                Debug.Log($"Điều chỉnh startIndex từ {currentIndex} thành {startIndex} (count: {count})");
+            }
+
+            var layoutInfo = CalculateLayoutInfo(count);
+            UpdateAvatarPositions(count, startIndex, layoutInfo);
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+    private bool ValidateUISetup(out int count)
+    {
+        count = _avtSpdUI?.Count ?? 0;
+        if (count == 0) return false;
+
+        ValidateRequiredComponents();
+        return true;
+    }
+
+    private (float offset, float fixedY) CalculateLayoutInfo(int count)
+    {
+        var fixedY = _avtSpdUI[0]?.GetComponent<RectTransform>()?.anchoredPosition.y ?? 0f;
         var totalGroupWidth = (count - 1) * avatarSpacing;
-        var poolRect = characterPool.GetComponent<RectTransform>();
-        var poolWidth = poolRect.rect.width;
         
+        var poolRect = characterPool.GetComponent<RectTransform>();
+        if (poolRect == null)
+            throw new System.NullReferenceException("poolRect RectTransform is null");
+            
+        var poolWidth = poolRect.rect.width;
         var offset = (poolWidth - totalGroupWidth) / 2;
 
+        return (offset, fixedY);
+    }
+
+    private void UpdateAvatarPositions(int count, int startIndex, (float offset, float fixedY) layoutInfo)
+    {
         for (var i = 0; i < count; i++)
         {
             var index = (startIndex + i) % count;
-            var avatarRect = _avtSpdUI[index].GetComponent<RectTransform>();
-            var targetPos = new Vector2(offset + i * avatarSpacing, fixedY);
-            avatarRect.anchoredPosition = targetPos;
-            var isFocused = Gameplay.Characters[index].IsMainCharacter;
-            var targetScale = isFocused ? focusedScale : unfocusedScale;
-            _avtSpdUI[index].transform.localScale = new Vector3(targetScale, targetScale, 1f);
-            _avtSpdUI[index].SetupUI(Gameplay.Characters[index], this, false);
+            if (!ValidateAvatarIndex(index)) continue;
+
+            UpdateSingleAvatarPosition(i, index, layoutInfo);
         }
+    }
+
+    private bool ValidateAvatarIndex(int index)
+    {
+        if (index >= _avtSpdUI.Count || _avtSpdUI[index] == null)
+        {
+            Debug.LogException(new System.IndexOutOfRangeException($"Invalid index {index} or null AVT_SpdUI"));
+            return false;
+        }
+
+        if (index >= Gameplay.Characters.Count || Gameplay.Characters[index] == null)
+        {
+            Debug.LogException(new System.IndexOutOfRangeException($"Invalid character index {index} or null Character"));
+            return false;
+        }
+
+        return true;
+    }
+
+    private void UpdateSingleAvatarPosition(int i, int index, (float offset, float fixedY) layoutInfo)
+    {
+        var avatarRect = _avtSpdUI[index].GetComponent<RectTransform>();
+        if (avatarRect == null)
+        {
+            Debug.LogException(new System.NullReferenceException($"RectTransform missing on AVT_SpdUI at index {index}"));
+            return;
+        }
+
+        var targetPos = new Vector2(layoutInfo.offset + i * avatarSpacing, layoutInfo.fixedY);
+        avatarRect.anchoredPosition = targetPos;
+
+        var isFocused = Gameplay.Characters[index].IsMainCharacter;
+        var targetScale = isFocused ? focusedScale : unfocusedScale;
+        _avtSpdUI[index].transform.localScale = new Vector3(targetScale, targetScale, 1f);
+        _avtSpdUI[index].SetupUI(Gameplay.Characters[index], this, false);
     }
 
     private void ClearAllChildren()
@@ -94,10 +240,32 @@ public class TopBar_UI : MonoBehaviour
         }
     }
 
-    public void DestroyAvt(AVT_SpdUI avt)
+    private void ReorderAvatars()
     {
-        if (avt == null || !_avtSpdUI.Remove(avt)) return;
-        Destroy(avt.gameObject);
-        SetUI();
+        var newOrder = new List<AVT_SpdUI>();
+        
+        foreach (var character in Gameplay.Characters)
+        {
+            var avatar = _avtSpdUI.FirstOrDefault(avt => 
+            {
+                try 
+                {
+                    return avt != null && avt.Character != null && avt.Character == character;
+                }
+                catch (System.Exception)
+                {
+                    return false;
+                }
+            });
+
+            if (avatar != null)
+            {
+                newOrder.Add(avatar);
+            }
+        }
+
+        _avtSpdUI.Clear();
+        _avtSpdUI.AddRange(newOrder);
     }
+    #endregion
 }
